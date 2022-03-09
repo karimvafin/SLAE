a5c43c5c0d2735dec77b7e4b240b3f1c94297480
karimvafin <karim.vafin@mail.ru>
2022-03-09 15:29:07 +0300

    Add new method: simple iteration

--- sem_1/src/project/solvers/SimpleIteration.hpp
+++ sem_1/src/project/solvers/SimpleIteration.hpp
@@ -4,8 +4,56 @@
 
 #ifndef SLAE_SIMPLEITERATION_HPP
 #define SLAE_SIMPLEITERATION_HPP
+
+#include "project/SlaeBaseException.hpp"
 #include "../sparse/CSR.hpp"
+#include "project/utility/Norm.hpp"
+#include "project/utility/overloads.hpp"
+
 template<typename T>
-std::vector<T> SimpleIteration(const CSR<T> &A, const std::vector<T> &b, const T &tao);
+std::vector<T> SimpleIteration(const CSR<T> &A, const std::vector<T> &b, const std::vector<T> &initialState,
+                               const T &tao, const T &tolerance)
+{
+#ifndef NDEBUG
+    if (A.get_row_size() != b.size())
+    {
+        std::stringstream buff;
+        buff << "Matrix and column dimensions are not equal! Matrix size: " << A.get_row_size() << "x" << A.get_col_size()
+        << ". Column size: "<< b.size() << ". File: " << __FILE__ << ". Line: " << __LINE__;
+
+        throw Slae::SlaeBaseExceptionCpp(buff.str());
+    }
+
+    if (A.get_row_size() != A.get_col_size())
+    {
+        std::stringstream buff;
+        buff << "Matrix dimensions are not equal! Matrix size: " << A.get_row_size() << "x" << A.get_col_size()
+             << ". File: " << __FILE__ << ". Line: " << __LINE__;
+
+        throw Slae::SlaeBaseExceptionCpp(buff.str());
+    }
+
+    if (initialState.size() != b.size())
+    {
+        std::stringstream buff;
+        buff << "Vector of constant terms and initial state vector dimensions are not equal! Vector b size: "
+             << b.size() << ". Initial state column size: "<< initialState.size() << ". File: "
+             << __FILE__ << ". Line: " << __LINE__;
+
+        throw Slae::SlaeBaseExceptionCpp(buff.str());
+    }
+#endif //NDEBUG
+
+    std::vector<T> currentState = initialState;
+    std::vector<T> r = A * currentState - b;
+
+    while (norm(r, NormType::ThirdNorm) > tolerance)
+    {
+        currentState = currentState - tao * r;
+        r = A * currentState - b;
+    }
+
+    return currentState;
+}
 
 #endif//SLAE_SIMPLEITERATION_HPP
