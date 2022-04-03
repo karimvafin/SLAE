b5056ca1dd3ca72f59e5f921b12fe8cab2b62d4d
klauchek <kniazeva.ms@phystech.edu>
2022-04-04 00:51:00 +0300

    Kinda finished (multiplying optimization + now  using copy ctor

--- task_1/Givens.hpp
+++ task_1/Givens.hpp
@@ -12,22 +12,23 @@ std::pair<DenseMatrix<T>, DenseMatrix<T>> GivensRotation(DenseMatrix<T>& A)
 {
     DenseMatrix<T> Q(A.get_row_size(), A.get_row_size(), 0);
     DenseMatrix<T> B_res(A.get_row_size(), A.get_col_size(), 0);
+    DenseMatrix<T> R = A;
+    //R = A;
+
     for (int k = 0; k < B_res.get_col_size(); ++k)
         B_res(k, k) = 1;
-    //std::vector<DenseMatrix<T>> B_arr;
-    //вектор матриц T вращений и потом и[ перемножить??
 
     T sin, cos;
     T upper, lower;
-    //на каждом шаге у нас будет зануляться один столбец
-    for(size_t step = 0; step < A.get_col_size(); ++step)
-    {
-        for (size_t i = step + 1; i < A.get_row_size(); ++i) //идем по строкам; они сравниваются как step и step + i
-        {
-            cos = A(step, step) / sqrt(A(step, step) * A(step, step) + A(i, step) * A(i, step));
-            sin = A(i, step) / sqrt(A(step, step) * A(step, step) + A(i, step) * A(i, step));
 
-            DenseMatrix<T> B(A.get_row_size(), A.get_col_size(), 0);
+    for(size_t step = 0; step < R.get_col_size(); ++step)
+    {
+        for (size_t i = step + 1; i < R.get_row_size(); ++i)
+        {
+            cos = R(step, step) / sqrt(R(step, step) * R(step, step) + R(i, step) * R(i, step));
+            sin = R(i, step) / sqrt(R(step, step) * R(step, step) + R(i, step) * R(i, step));
+
+            DenseMatrix<T> B(R.get_row_size(), R.get_col_size(), 0);
             for (int k = 0; k < B.get_col_size(); ++k)
                 B(k, k) = 1;
             B(step, step) = cos;
@@ -35,27 +36,24 @@ std::pair<DenseMatrix<T>, DenseMatrix<T>> GivensRotation(DenseMatrix<T>& A)
             B(i, step) = -sin;
             B(step, i) = sin;
 
-            for (size_t j = 0; j < B_res.get_row_size(); ++j)//идем по столбцам;
+            for (size_t j = 0; j < B_res.get_row_size(); ++j)//multiplying
             {
                 upper = cos * B_res(step, j) + sin * B_res(i, j);
                 lower = -sin * B_res(step, j) + cos * B_res(i, j);
 
-                if(fabs(lower) < tolerance)
-                    lower = 0;
                 B_res(step, j) = upper;
                 B_res(i, j) = lower;
             }
-            //B_arr.push_front(B);
 
-            for (size_t j = step; j < A.get_row_size(); ++j)//идем по столбцам;
+            for (size_t j = step; j < R.get_row_size(); ++j)
             {
-                upper = cos * A(step, j) + sin * A(i, j);
-                lower = -sin * A(step, j) + cos * A(i, j);
+                upper = cos * R(step, j) + sin * R(i, j);
+                lower = -sin * R(step, j) + cos * R(i, j);
 
                 if(fabs(lower) < tolerance)
                     lower = 0;
-                A(step, j) = upper;
-                A(i, j) = lower;
+                R(step, j) = upper;
+                R(i, j) = lower;
             }
         }
     }
@@ -67,7 +65,7 @@ std::pair<DenseMatrix<T>, DenseMatrix<T>> GivensRotation(DenseMatrix<T>& A)
             Q(i, j) = B_res(j, i);
     }
 
-    std::pair<DenseMatrix<T>, DenseMatrix<T>> QR(Q, A);
+    std::pair<DenseMatrix<T>, DenseMatrix<T>> QR(Q, R);
     return QR;
 }
 
