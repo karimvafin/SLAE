f7725f4eaceea69b38977127e621543053cea3da
klauchek <kniazeva.ms@phystech.edu>
2022-04-03 23:43:34 +0300

    Third vers

--- task_1/Givens.hpp
+++ task_1/Givens.hpp
@@ -3,13 +3,18 @@
 
 #include "../sem_1/src/project/utility/overloads.hpp"
 #include "../sem_1/src/project/dense/densematrix.hpp"
+#include <cmath>
+
+#define tolerance 10e-15
 
 template<typename T>
 std::pair<DenseMatrix<T>, DenseMatrix<T>> GivensRotation(DenseMatrix<T>& A)
 {
     DenseMatrix<T> Q(A.get_row_size(), A.get_row_size(), 0);
-    //DenseMatrix<T> R(A.get_row_size(), A.get_col_size(), 0);
-    std::vector<DenseMatrix<T>> B_arr;
+    DenseMatrix<T> B_res(A.get_row_size(), A.get_col_size(), 0);
+    for (int k = 0; k < B_res.get_col_size(); ++k)
+        B_res(k, k) = 1;
+    //std::vector<DenseMatrix<T>> B_arr;
     //вектор матриц T вращений и потом и[ перемножить??
 
     T sin, cos;
@@ -25,33 +30,41 @@ std::pair<DenseMatrix<T>, DenseMatrix<T>> GivensRotation(DenseMatrix<T>& A)
             DenseMatrix<T> B(A.get_row_size(), A.get_col_size(), 0);
             for (int k = 0; k < B.get_col_size(); ++k)
                 B(k, k) = 1;
-            B(step, step) = B(i, i) = cos;
+            B(step, step) = cos;
+            B(i, i) = cos;
             B(i, step) = -sin;
             B(step, i) = sin;
 
-            B_arr.push_front(B);
+            for (size_t j = 0; j < B_res.get_row_size(); ++j)//идем по столбцам;
+            {
+                upper = cos * B_res(step, j) + sin * B_res(i, j);
+                lower = -sin * B_res(step, j) + cos * B_res(i, j);
 
-            for (size_t j = step + 1; j < A.get_row_size(); ++j)//идем по столбцам;
+                if(fabs(lower) < tolerance)
+                    lower = 0;
+                B_res(step, j) = upper;
+                B_res(i, j) = lower;
+            }
+            //B_arr.push_front(B);
+
+            for (size_t j = step; j < A.get_row_size(); ++j)//идем по столбцам;
             {
                 upper = cos * A(step, j) + sin * A(i, j);
                 lower = -sin * A(step, j) + cos * A(i, j);
 
-                //if(Tabs(lower) < tolerance<T>)
-                //    lower = 0;
+                if(fabs(lower) < tolerance)
+                    lower = 0;
                 A(step, j) = upper;
                 A(i, j) = lower;
             }
         }
     }
 
-    DenseMatrix<T> B_res(A.get_row_size(), A.get_col_size(), 0);
-    for(int i = 0; i < B_arr.size(); ++i)
-        B_res *= B_arr[i];
 
-    for (size_t i = 0; i < A.get_row_size(); ++i)
+    for (size_t i = 0; i < B_res.get_row_size(); ++i)
     {
-        for (size_t j = 0; j < A.get_col_size(); ++j)
-            Q(i, j) = A(j, i);
+        for (size_t j = 0; j < B_res.get_col_size(); ++j)
+            Q(i, j) = B_res(j, i);
     }
 
     std::pair<DenseMatrix<T>, DenseMatrix<T>> QR(Q, A);
